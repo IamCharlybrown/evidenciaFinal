fa81e95fd8d6e4d3dadfb5fd3b206f11e0691937
ImCharlyBrown <al02910748@tecmilenio.mx>
2025-04-26 16:47:43 -0600

    factory order

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -16,6 +16,8 @@ class OrderFactory extends Factory
             'user_id' => User::factory(),
             'status' => $this->faker->randomElement(['pendiente', 'pagada', 'cancelada', 'entregada']),
             'total' => $this->faker->randomFloat(2, 20, 500),
+            'initial_image' => $this->faker->imageUrl(640, 480, 'business', true, 'Initial'),
+            'delivery_image' => $this->faker->imageUrl(640, 480, 'transport', true, 'Delivery'),
         ];
     }
 }
